a9a650a01f5639c2138dcf78a7cf6baba331d9e9
Apurva Mukherjee <apurvamukherjee@Apurvas-MacBook-Pro.local>
2025-07-05 17:52:44 +0530

    Hashtable operations

---
+++ HashTable/README.md
@@ -0,0 +1,6 @@
+## HashTable
+
+## Big O
+
+1. **set**: Set a key and value: **O(1)**
+2. **get**: Get the value for the key :  **O(1)**
---
+++ HashTable/hashTable.js
@@ -0,0 +1,59 @@
+// Hash Table: [ 
+// [
+//      ['key1', 10], ['key2', 20]
+// ], 
+// [
+//      ['key3', 10], ['key4', 30]
+// ] 
+//]
+class HashTable {
+    constructor(size = 7){
+        this.dataMap = new Array(size);
+    }
+
+    // To verify the table data
+    printTable() {
+        for(let i = 0; i < this.dataMap.length; i++){
+            console.log(`${i}: ${this.dataMap[i]}`);
+        }
+    }
+
+    // For a given key, generate an index for hashtable implemented using a nested array
+    #hash(key) {
+        let hash = 0;
+        for(let i = 0; i < key.length; i++){
+            hash = (hash + key.charCodeAt(i) * 23) % this.dataMap.length;
+        }
+        return hash;
+    }
+
+    // Adds a key and value to the hashTable in the index obtained using hash function
+    set(key, value){
+        const index = this.#hash(key);
+        if(!this.dataMap[index]) this.dataMap[index] = [];
+
+        this.dataMap[index].push([key, value]);
+        return this;
+    }
+
+    // Returns the value for the key in hashtable
+    get(key) {
+        const index = this.#hash(key);
+        if(this.dataMap[index]){
+            for(let i = 0; i < this.dataMap[index].length; i++){
+                if(this.dataMap[index][i][0] === key){
+                    return this.dataMap[index][i][1];
+                }
+            }
+        }
+        return undefined;
+    }
+}
+
+const hashTable = new HashTable();
+hashTable.set('apple', 20);
+hashTable.set('mangoes', 10);
+
+console.log(hashTable.get('mangoes'));
+
+hashTable.printTable()
