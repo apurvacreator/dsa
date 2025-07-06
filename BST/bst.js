c3690d20e12904500bb080f13c2a5322b4f07271
Apurva Mukherjee <apurvamukherjee@Apurvas-MacBook-Pro.local>
2025-07-06 07:19:57 +0530

    BST recursive operations

--- BST/bst.js
+++ BST/bst.js
@@ -62,6 +62,72 @@ class BST {
         }
         return false;
     }
+
+    // Checks if value exists in BST recursively
+    rContains(value, currentNode = this.root) {
+        if(currentNode === null) return false;
+        if(value === currentNode.value) return true;
+
+        if(value < currentNode.value) {
+            return this.rContains(value, currentNode.left);
+        } else {
+            return this.rContains(value, currentNode.right);
+        }
+    }
+
+    // Inserts a new leaf node to BST
+    #rInsert(value, currentNode = this.root) {
+        if(currentNode === null) return new Node(value);
+
+        if(value < currentNode.value) {
+            currentNode.left =  this.#rInsert(value, currentNode.left);
+        }else if(value > currentNode.value){
+            currentNode.right = this.#rInsert(value, currentNode.right);
+        }
+
+        return currentNode;
+    }
+
+    rInsert(value) {
+        if(!this.root) {
+            this.root = new Node(value);
+        }
+        this.#rInsert(value);
+    }
+
+    minValue(currentNode) {
+        while(currentNode.left !== null) {
+            currentNode = currentNode.left;
+        }
+        return currentNode.value;
+    }
+
+    #rDelete(value, currentNode){
+        if(currentNode === null) return null;
+        if(value < currentNode.value) {
+            currentNode.left = this.#rDelete(value, currentNode.left);
+        }else if(value > currentNode.value){
+            currentNode.right = this.#rDelete(value, currentNode.right);
+        }else {
+            if(currentNode.left === null && currentNode.right === null){
+                currentNode = null;
+            }else if(currentNode.left === null) {
+                currentNode = currentNode.right;
+            }else if(currentNode.right === null) {
+                currentNode = currentNode.left;
+            }else {
+                const subtreeMin = this.minValue(currentNode);
+                console.log('min value', subtreeMin);
+                currentNode.value = subtreeMin;
+                currentNode.right = this.#rDelete(subtreeMin, currentNode.right);
+            }
+        }
+        return currentNode;
+    }
+
+    rDelete(value) {
+        this.root = this.#rDelete(value, this.root);
+    }
 }
 
 const bst = new BST();
@@ -71,6 +137,18 @@ bst.insert(29);
 bst.insert(75);
 bst.insert(56);
 bst.insert(82);
-console.log(bst.contains(31)); // true
-console.log(bst.contains(45)); // false
+console.log('contains 31 -->', bst.contains(31)); // true
+console.log('contains 45 -->', bst.contains(45)); // false
 
+console.log('rContains 45 -->', bst.rContains(45)); // false
+console.log('rContains 56 -->', bst.rContains(56)); // true
+console.log('rDelete 56 -->');
+bst.rDelete(56);
+console.log('rContains 56 -->', bst.rContains(56)); // false
+console.log('BST Root ', bst.root);
+
+console.log('rDelete root -->');
+bst.rDelete(47);
+
+console.log('rContains 47 -->', bst.rContains(47)); // false
+console.log('BST Root ', bst.root.value);
