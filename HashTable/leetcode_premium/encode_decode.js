49e1ac03d7525db5b282b00e875e827e51b456a8
Apurva Mukherjee <apurvamukherjee@Apurvas-MacBook-Pro.local>
2025-07-19 10:19:27 +0530

    encode decode algo

---
+++ HashTable/leetcode_premium/encode_decode.js
@@ -0,0 +1,42 @@
+function encode(arr) {
+  for (let i = 0; i < arr.length; i++) {
+    arr[i] = `${arr[i].length}#${arr[i]}`;
+  }
+  return arr.join("");
+}
+
+const input = ["neet", "code", "love", "you"];
+console.log("input =>", input);
+const encoded = encode(input);
+console.log("encoded =>", encoded);
+
+function decode(encoded) {
+  let decoded = [];
+  //   let j = 0;
+  //   for (let i = 0; i < encoded.length; i++) {
+  //     let count = +encoded[i];
+  //     i = i + 2;
+  //     decoded[j] = encoded.substring(i, i + count);
+  //     i = i + count - 1;
+  //     j++;
+  //   }
+  //   return decoded;
+
+  let i = 0;
+
+  while (i < encoded.length) {
+    let j = i;
+    while (encoded[j] !== "#") {
+      j++;
+    }
+    let length = +encoded.substring(i, j);
+    i = j + 1;
+    j = i + length;
+    decoded.push(encoded.substring(2, 6));
+    i = j;
+  }
+  return decoded;
+}
+
+const decoded = decode(encoded);
+console.log("decoded =>", decoded);
